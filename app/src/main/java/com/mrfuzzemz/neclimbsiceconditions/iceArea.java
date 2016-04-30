d116f365c2e64f294c47b01f413d281678e6b313
mrfuzzemz <mrfuzzemz@gmail.com>
2016-04-30 14:17:52 -0400

    .gitignore is now working

---
+++ app/src/main/java/com/mrfuzzemz/neclimbsiceconditions/iceArea.java
@@ -0,0 +1,15 @@
+package com.mrfuzzemz.neclimbsiceconditions;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class iceArea {
+	public String updateText;
+	public LatLng location;
+	public String verdict;
+	
+	
+	
+	public iceArea(){
+		
+	}
+}
